d2a41f61118b0d83b925c40fcc8e9331a8a19d59
Ian Gagorik <ian.gagorik@gmail.com>
2017-11-25 18:58:28 -0500

    Cell edge cases and test cleanup.

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,10 @@
 module GameOfLife
-
   class Game
-
     def play(world)
       next_world = []
-      world.each do |row|
+      world.each_with_index do |row, row_index|
         new_row = []
-        row.each do |cell|
+        row.each_with_index do |cell, column_index|
           new_cell = 0
           new_row << new_cell
         end
@@ -16,19 +14,20 @@ module GameOfLife
     end
 
     def cell_is_alive?(row, column, world)
+      return false if row < 0 || column < 0 || row >= world.length || column >= world[row].length
       world[row][column] == 1
     end
 
     def neighbour_count(row, column, world)
       neighbours = 0
-      neighbours += 1 if cell_is_alive?(row-1, column-1, world)
-      neighbours += 1 if cell_is_alive?(row-1, column, world)
-      neighbours += 1 if cell_is_alive?(row-1, column+1, world)
-      neighbours += 1 if cell_is_alive?(row, column-1, world)
-      neighbours += 1 if cell_is_alive?(row, column+1, world)
-      neighbours += 1 if cell_is_alive?(row+1, column-1, world)
-      neighbours += 1 if cell_is_alive?(row+1, column, world)
-      neighbours += 1 if cell_is_alive?(row+1, column+1, world)
+      neighbours += 1 if cell_is_alive?(row - 1, column - 1, world)
+      neighbours += 1 if cell_is_alive?(row - 1, column, world)
+      neighbours += 1 if cell_is_alive?(row - 1, column + 1, world)
+      neighbours += 1 if cell_is_alive?(row, column - 1, world)
+      neighbours += 1 if cell_is_alive?(row, column + 1, world)
+      neighbours += 1 if cell_is_alive?(row + 1, column - 1, world)
+      neighbours += 1 if cell_is_alive?(row + 1, column, world)
+      neighbours += 1 if cell_is_alive?(row + 1, column + 1, world)
       neighbours
     end
   end
